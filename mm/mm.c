767c60a2fac3365815146ff0310b21884e6b0bf1
harihitode <harihitode@gmail.com>
2022-10-31 22:56:54 +0900

    add fma as a inline assembler

--- mm/mm.c
+++ mm/mm.c
@@ -8,6 +8,15 @@
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
+float fmaf(float a, float b, float c) {
+  float f;
+  asm volatile("fmadd.s %0, %1, %2, %3"
+               : "=f"(f)
+               : "f"(a), "f"(b), "f"(c)
+               );
+  return f;
+}
+
 static void mm_naive(size_t m, size_t n, size_t p,
                             t* a, size_t lda, t* b, size_t ldb, t* c, size_t ldc)
 {
